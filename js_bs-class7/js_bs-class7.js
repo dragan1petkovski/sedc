5522ab0e696763b13f0b1502ddda34e8646c0039
dragan1petkovski <dragan_petkovski@hotmail.com>
2024-01-06 09:10:57 +0100

    update homework for Javascript basic Class 7

--- js_bs-class7/js_bs-class7.js
+++ js_bs-class7/js_bs-class7.js
@@ -1,11 +1,27 @@
-let animaldatabase = {};
+//----------------------- Homework 1 - animal -----------------------
+document.getElementById("animalspeak-input").addEventListener("click",animalspeak);
+
 let animal = {
-    name: "",
-    kind: "",
-    sound: "",
-    speak: function() {
-        return `${this.kind} Says: ${this.sound}`
+    kind: '',
+    speak: function(textoutput,message) {
+        textoutput.innerHTML = '';
+        let animalsays = document.createElement("p");
+        let boldstatement = document.createElement("strong");
+        this.kind = `${this.kind} says:`
+        boldstatement.appendChild(document.createTextNode(this.kind.toUpperCase()));
+        animalsays.appendChild(boldstatement);
+        message = `: ${message}!!`
+        animalsays.appendChild(document.createTextNode(message))
+        textoutput.appendChild(animalsays);
+        let = output = `${this.kind} ${message}`
+        console.log(output)
 
     }
-};
+}
+
+function animalspeak()
+{
+    animal.kind = document.getElementById("animalkind").value;
+    animal.speak(document.getElementById("animalspeak-output"),document.getElementById("animalspeak-text").value)
+}
 
