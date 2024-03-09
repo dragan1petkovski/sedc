6e8396d3ff0bccfd1f7f86d9ba6713bd7d07f01a
dragan1petkovski <dragan_petkovski@hotmail.com>
2024-03-10 00:23:37 +0100

    Upload homework from class 4

---
+++ c#-bs-class4/homework/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int flagnumber;
+            
+            Console.WriteLine("For Task 1 type '1'\nFor Task 4 type '2'");
+            flagnumber = utilities.enterNumber("Enter number 1 or 2"); 
+            if( flagnumber == 1)
+            {
+                Console.WriteLine("Start Task 1: Write 'N' to stop entering numbers");
+                int[] arraysOfNumbers = new int[1];
+                int tempnumber;
+                string tempstring;
+                int counter = 0;
+                while (true)
+                {
+                    Console.WriteLine("Enter Number");
+                    tempstring = Console.ReadLine();
+                    if (utilities.CheckForValidInput(tempstring))
+                    {
+                        if (Int32.TryParse(tempstring, out tempnumber))
+                        {
+                            arraysOfNumbers[counter] = tempnumber;
+                            arraysOfNumbers = utilities.IncreaseArraySizeByOne(arraysOfNumbers);
+                            counter++;
+                        }
+                        else if (tempstring == "N")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong Number or Exit letter");
+                        }
+                    }
+                    
+                }
+                int sum = 0;
+                foreach(int i in arraysOfNumbers)
+                {
+                    if(i%2==0)
+                    {
+                        sum = sum + i;
+                    }
+                }
+                Console.WriteLine($"Sum of all even numbers is {sum}");
+
+            }
+            else if ( flagnumber == 2)
+            {
+                Console.WriteLine("Start Task 4");
+                string stringOfNames = "";
+                string tempstring;
+                string tempflag;
+
+                while (true)
+                {
+                    Console.WriteLine("Do you want to enter name? (Y/N)");
+                    tempflag = Console.ReadLine();
+                    if (tempflag.ToUpper() == "N")
+                    {
+                        break;
+                    }
+                    else if (tempflag.ToUpper() == "Y")
+                    {
+                        Console.WriteLine("Enter Name");
+                        tempstring = Console.ReadLine();
+                        stringOfNames += $"{tempstring},";
+                    }
+                }
+
+                foreach(string name in stringOfNames.Split(","))
+                { Console.WriteLine(name); }
+            }
+            else
+            {
+                Console.WriteLine("Enter number 1 or 2");
+            }
+
+        }
+
+    }
+}
---
+++ c#-bs-class4/homework/utilities.cs
@@ -0,0 +1,69 @@
+﻿namespace homework
+{
+    public class utilities
+    {
+        public static bool CheckForValidInput(string something)
+        {
+            if (!string.IsNullOrEmpty(something))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    
+        public static bool CheckMathOperator(char mathoperator)
+        {
+            switch (mathoperator)
+            {
+                case '+':
+                    return true;
+                case '-':
+                    return true;
+                case '*':
+                    return true;
+                case '/':
+                    return true;
+                case '=':
+                    return true;
+                default:
+                    Console.WriteLine("Wrong Operator: chose one from: +, -, *, /, =");
+                    return false;
+
+            }
+        }
+
+        public static int enterNumber(string exceptionmessage)
+        {
+            string number;
+            while (true)
+            {
+                Console.WriteLine("Enter Number");
+                number = Console.ReadLine();
+
+                if (CheckForValidInput(number))
+                {
+                    if(Int32.TryParse(number,out int output));
+                    {
+                        return output;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(exceptionmessage);
+                }
+            }
+
+
+        }
+    
+        public static int[] IncreaseArraySizeByOne (int[] ints)
+        {
+            Array.Resize(ref ints, ints.Length+1);
+            return ints;
+        }
+    }
+
+}
