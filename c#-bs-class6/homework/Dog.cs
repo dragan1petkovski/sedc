82cbedf7f59aafaa7262eb4b88f53094c6d5714a
dragan1petkovski <dragan_petkovski@hotmail.com>
2024-03-14 21:55:24 +0100

    Upload homework 6

---
+++ c#-bs-class6/homework/Dog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    internal class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color {get; set; }
+
+        public Dog()
+        { }
+
+        public Dog(string name, string breed, string color)
+        {
+            Name = name;
+            Breed = breed;
+            Color = color;
+        }
+
+        public void Eating()
+        {
+            Console.WriteLine($"{Name} is eating");
+        }
+
+        public void Playing()
+        {
+            Console.WriteLine($"{Name} is playing");
+        }
+
+        public override string ToString()
+        {
+            return $"Dog name: {Name}\nDog breed: {Breed}\nDog Color: {Color}";
+        }
+    }
+}
---
+++ c#-bs-class6/homework/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            while (true)
+            {
+                string flag;
+                Console.WriteLine("Add new dog (y/n)");
+                flag = Console.ReadLine();
+                if (flag.ToLower() == "y")
+                {
+                    Console.WriteLine("Enter dog name");
+                    string dogname = Console.ReadLine();
+
+                    Console.WriteLine("Enter dog breed");
+                    string dogbreed = Console.ReadLine();
+
+                    Console.WriteLine("Etner dog color");
+                    string dogcolor = Console.ReadLine();
+
+                    Dog newdog = new Dog(dogname, dogbreed, dogcolor);
+
+                    Console.WriteLine(newdog);
+                    continue;
+                }
+                else if (flag.ToLower() == "n")
+                {
+                    Console.WriteLine("Goodbye");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Write proper flag!!!");
+                }
+            }
+
+            
+        }
+    }
+}
